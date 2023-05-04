62826f7c036e5120b59b3b28950efe7292aa10f6
constantin jenaru <buc.constan300@gmail.com>
2023-05-04 15:36:00 +0300

    done

--- src/main/java/Forum.java
+++ src/main/java/Forum.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class Forum {
 
     private static Forum instance = new Forum();
-    private List<Topic> topics;
+    private static List<Topic> topics;
 
     Forum() {
         this.topics = new ArrayList<>();
@@ -29,6 +29,25 @@ public class Forum {
     }
 
     public static void main(String[] args) {
+        Topic topic1= new Topic("Number1", "first", TopicKind.NEWS);
+        Topic topic2= new Topic("Number2", "second", TopicKind.ANNOUNCEMENT);
+        Topic topic3= new Topic("Number3", "third", TopicKind.NEWS);
+        topics.add(topic1);
+        topics.add(topic2);
+        topics.add(topic3);
+        Comment comment1= new Comment("Comment1", topic1);
+        Comment comment2= new Comment("Comment2", topic2);
+        Comment comment3= new Comment("Comment3", topic1);
+        Comment comment4= new Comment("Comment4", topic3);
+        comment1.toggleModeration();
+        comment2.toggleModeration();
+        comment3.toggleModeration();
+        comment4.toggleModeration();
+        topic1.addComment(comment1);
+        topic1.addComment(comment3);
+        topic2.addComment(comment2);
+        topic3.addComment(comment4);
+
         getInstance().getEntireForum();
     }
 
